f4482e5c4cc2c4a9351b6630f9fe3965ab222e67
TMendes1 <tomas12001@hotmail.com>
2019-08-14 10:10:50 +0100

    Batman

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -s 'https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json' | jq -r '.[51].name'
+curl -s 'https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json' | jq '.[51].name'
