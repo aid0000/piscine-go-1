66a6fa2a4a8078db8eea6d14a294fd3bca751246
TMendes1 <tomas12001@hotmail.com>
2019-08-14 16:50:59 +0100

    fml

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -s 'https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json' | jq -r '.[ '${HERO_ID}-1' ].connections.relatives'
+curl -s 'https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json' | jq ".[] | select ( .id==$HERO_ID ) " | grep 'relatives' | cut -d'"' -f4
