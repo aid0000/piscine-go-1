ff296f4576c58b503c39f92c5a8793dc4135bc89
TMendes1 <tomas12001@hotmail.com>
2019-09-06 17:20:14 +0100

    m2

--- sqrt.go
+++ sqrt.go
@@ -1,12 +1,17 @@
-package piscine
-import (
-	"math"
-)
-func Sqrt(nb float64) float64 {
-flnb:=float64(nb)
-	if nb<0{
-		return 0
-	}
-	return math.Sqrt(flnb)
 
+package piscine
+
+func Sqrt(nb int) int {
+	a := 1
+
+	for nb != IterativePower(a, 2) {
+		a++
+		if a > nb {
+			return 0
+		}
+	}
+	if nb == IterativePower(a, 2) {
+		return a
+	}
+	return 0
 }
