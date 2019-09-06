e3726519abc5aa39055c6c265c7303862c5139fa
TMendes1 <tomas12001@hotmail.com>
2019-09-06 17:27:23 +0100

    m2

--- capitalize.go
+++ capitalize.go
@@ -1,22 +1,30 @@
 package piscine
+
+func prim(a rune) bool {
+
+	if (a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z') || (a >= '0' && a <= '9') {
+		return true
+	}
+	return false
+}
+
 func Capitalize(s string) string {
+	ar := []rune(s)
 
-	h := []rune(s)
-	result := ""
-	for i := 0; i <= lent(h)-1; i++ {
-		if (h[i] >= 0) && (h[i] <= 47) {
+	letra := true
 
-		
-		if (h[i+1] >= 'a') && (h[i+1] <= 'z') {
+	for i := 0; i < len(s); i++ {
+		if prim(ar[i]) == true && letra {
+			if ar[i] >= 'a' && ar[i] <= 'z' {
+				ar[i] = 'A' - 'a' + ar[i]
 
-			h[i+1] = h[i+1] - 32
-
-
+			}
+			letra = false
+		} else if ar[i] >= 'A' && ar[i] <= 'Z' {
+			ar[i] = 'a' - 'A' + ar[i]
+		} else if prim(ar[i]) == false {
+			letra = true
 		}
 	}
-		result += string(h[i])
-	
-	}
-	return result
-
+	return string(ar)
 }
