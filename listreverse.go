b56d14b35ddfdead88de03b99ca561ecd67319f3
MSilva95 <53977751+MSilva95@users.noreply.github.com>
2019-09-08 15:54:06 +0100

    P

--- listreverse.go
+++ listreverse.go
@@ -1,17 +1,17 @@
-package piscine
+Package piscine
 
 func ListReverse(l *List) {
-
 	current := l.Head
 	prev := l.Head
 	prev = nil
-
 	for current != nil {
 		next := current.Next
 		current.Next = prev
 		prev = current
 		current = next
 	}
-	l.Head = prev
+	temp := l.Head
+	l.Head = l.Tail
+	l.Tail = temp
 }
 
