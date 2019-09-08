7e1cd8dca5ca253a8e0e07bdf4982cbcb7531478
MSilva95 <53977751+MSilva95@users.noreply.github.com>
2019-09-08 13:23:09 +0100

    F

--- displayfile/displayfile.go
+++ displayfile/displayfile.go
@@ -8,14 +8,14 @@ import (
 
 func main(){
 	if piscine.Lent3(os.Args)==1{
-		fmt.Println("File name missing")
+		fmt.Println("File name missing\n")
 	}else if  piscine.Lent3(os.Args)>2{
-		fmt.Println("Too many arguments")
+		fmt.Println("Too many arguments\n")
 	}else if os.Args[1] == "quest8.txt" {
 
 		data, err := ioutil.ReadFile("quest8.txt")
     if err != nil {
-		fmt.Printf(err.Error(),\n)
+		fmt.Printf(err.Error())
         return
     }
     fmt.Println(string(data))
