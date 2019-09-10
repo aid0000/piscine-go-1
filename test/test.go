957376f2e38f6a2301c0165129be6ff26ba80744
TMendes1 <tomas12001@hotmail.com>
2019-09-10 15:52:43 +0100

    n

--- test/test.go
+++ test/test.go
@@ -1,8 +1,9 @@
 package main
 
+
 import (
-       "fmt"
-       piscine ".."
+	"fmt"
+	piscine ".."
 )
 
 func main() {
@@ -10,6 +11,32 @@ func main() {
 	piscine.BTreeInsertData(root, "1")
 	piscine.BTreeInsertData(root, "7")
 	piscine.BTreeInsertData(root, "5")
-	piscine.BTreeApplyPostorder(root, fmt.Println)
+	selected := piscine.BTreeSearchItem(root, "7")
+	fmt.Print("Item selected -> ")
+	if selected != nil {
+		fmt.Println(selected.Data)
+	} else {
+		fmt.Println("nil")
+	}
 
+	fmt.Print("Parent of selected item -> ")
+	if selected.Parent != nil {
+		fmt.Println(selected.Parent.Data)
+	} else {
+		fmt.Println("nil")
+	}
+
+	fmt.Print("Left child of selected item -> ")
+	if selected.Left != nil {
+		fmt.Println(selected.Left.Data)
+	} else {
+		fmt.Println("nil")
+	}
+
+	fmt.Print("Right child of selected item -> ")
+	if selected.Right != nil {
+		fmt.Println(selected.Right.Data)
+	} else {
+		fmt.Println("nil")
+	}
 }
