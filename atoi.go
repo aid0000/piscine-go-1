6489ca6cbc2b273b2e8f27d11bb37d0e133c2ac2
TMendes1 <tomas12001@hotmail.com>
2019-09-10 12:24:38 +0100

    A

--- atoi.go
+++ atoi.go
@@ -46,7 +46,8 @@ func convert(sign int, absStr []rune) int {
 }
 func trimspace1(a []rune) []rune{
 	for i:=0;i<=lent(a)-1;i++{
-		if a[i]==' '|| a[i]=='.'{
+
+		if a[i]==' '|| a[i]=='.'|| a[i]>=56{
 			return []rune {'0'}
 		}
 	}
