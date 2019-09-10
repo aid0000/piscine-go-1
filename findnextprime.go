405677c1a1b05e0917d64b7cbffe81a79a7ec3fd
TMendes1 <tomas12001@hotmail.com>
2019-09-10 10:06:03 +0100

    m,

--- findnextprime.go
+++ findnextprime.go
@@ -1,6 +1,9 @@
 package piscine
 
 func FindNextPrime(nb int) int {
+	if nb>2147483647{
+		return 0
+	}
 	if IsPrime(nb) {
 		return nb
 	} else {
@@ -8,3 +11,8 @@ func FindNextPrime(nb int) int {
 	}
 }
 
+/*int8   : -128 to 127 
+int16  : -32768 to 32767 
+						
+int32  : -2147483648 to 2147483647 
+int64  : -9223372036854775808 to 9223372036854775807*/
