ca6063640ada0114184397f260c613656871df29
TMendes1 <tomas12001@hotmail.com>
2019-09-10 10:00:22 +0100

    m,

--- findnextprime.go
+++ findnextprime.go
@@ -1,42 +1,10 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	if isPrime2(nb) {
+	if IsPrime(nb) {
 		return nb
 	} else {
 		return FindNextPrime(nb+1) 
 	}
 }
-func isPrime2(value int) bool {
 
-    if value<=1{
-        return false
-    }
-      for i:=2; i<value; i++{
-         if value % i == 0{
-          return false
-        }
-    }
-    return true
-}
-
-
-
-func IsPrime2(value int) bool {
-	if value < 2 {
-		return false
-	}
-	b:=string(value)
-	a:=[]rune(b)
-	for p,_:=range a{
-
-		i := 2
-		for i <= p {
-			if value%i == 0 {
-				return false
-			}
-			i++
-		}
-	}
-	return true
-}
