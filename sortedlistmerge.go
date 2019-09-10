c18d2fa1aa0e851ec73924793c01f2f7871fef62
TMendes1 <tomas12001@hotmail.com>
2019-09-10 11:30:23 +0100

    cal

--- sortedlistmerge.go
+++ sortedlistmerge.go
@@ -5,14 +5,14 @@ func SortedListMerge(l1 *NodeI, l2 *NodeI) *NodeI {
 	l1 = ListSort(l1)
 	l2 = ListSort(l2)
 
-	if l1 == nil {
+	if l1 == nil {    
 		return l2
-	}
+	}								//se tiverem vazias retorna a que tem algo
 	if l2 == nil {
 		return l1
 	}
 	if l1.Data <= l2.Data {
-		l1.Next = SortedListMerge(l1.Next, l2)
+		l1.Next = SortedListMerge(l1.Next, l2)		
 		return l1
 	} else {
 		l2.Next = SortedListMerge(l1, l2.Next)
