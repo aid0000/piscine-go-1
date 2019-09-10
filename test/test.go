a1cd53612062c854dfc37a4710bc6ed11486bfc0
TMendes1 <tomas12001@hotmail.com>
2019-09-10 15:00:08 +0100

    #

--- test/test.go
+++ test/test.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	
-	"github.com/01-edu/z01"
-	piscine ".."
+    "fmt"
+    piscine ".."
 )
 
 func main() {
-	piscine.PrintNbrBase(125, "0123456789")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(-125, "01")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(125, "0123456789ABCDEF")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(-125, "choumi")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(125, "aa")
-	z01.PrintRune('\n')
+     root := &piscine.TreeNode{Data: "4"}
+     piscine.BTreeInsertData(root, "1")
+     piscine.BTreeInsertData(root, "7")
+     piscine.BTreeInsertData(root, "5")
+     fmt.Println(root.Left.Data)
+     fmt.Println(root.Data)
+     fmt.Println(root.Right.Left.Data)
+     fmt.Println(root.Right.Data)
+
 }
