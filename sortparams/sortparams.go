f1666ea7bafd492d0d3d4592e0d62d7d24b567dc
TMendes1 <tomas12001@hotmail.com>
2019-09-10 14:31:32 +0100

    s

--- sortparams/sortparams.go
+++ sortparams/sortparams.go
@@ -1,35 +1,67 @@
 package main
 
-//import (
-  //  piscine ".."
-	//"os"
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	array := []string(os.Args[1:])
+
+	quicksort(array)
+	for i := 0; i < len(array); i++ {
+
+		fmt.Println(array[i])
+	}
+}
+
+
+func quicksort(table []string) {
+	beg := 0
+	end := len(table) - 1
+
+	Srot2(table, beg, end)
+}
+func Srot2(table []string, beg int, end int) {
+
+	if beg < end {
+		lockPivo := move(table, beg, end)
+		Srot2(table, beg, lockPivo-1)
+		Srot2(table, lockPivo+1, end)
+
+	}
+}
+
+func move(table []string, beg int, end int) int {
+	pivo := table[end]
+	i := beg - 1
+
+	for j := beg; j < end; j++ {
+		if table[j] <= pivo {
+			i++
+			aux := table[j]
+			table[j] = table[i]
+			table[i] = aux
+		}
+	}
+
+	aux := table[end]
+	table[end] = table[i+1]
+	table[i+1] = aux
+
+	return i + 1
+}
+
+
+
+
+
+
+
+
+
+
+
+
 	
-//)
 
-func main() {/*
-	l:=lent(os.Args)
-	for i:=0; i<l; i++{
-		
-		piscine.Printrunes(os.Args[i], 0)
-	}
-}
-
-
-
-   for  i:=0;i<L;i++{
-
-      for j:=0;j< (L-1-i);j++{
-         if items[j] > items[j+1]{
-            items[j], items[j+1] = items[j+1], items[j]
-         }
-      }
-   }
-
-}
-func lent(d []string) int{
-	inc:=0
-	for _, _= range d{
-		inc ++
-	}
-	 return inc*/
-}
