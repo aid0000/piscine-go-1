0914696e194ae58a52f9a7b3362f07ae5d441c8a
TMendes1 <tomas12001@hotmail.com>
2019-09-11 16:08:12 +0100

    mm

--- findnextprime.go
+++ findnextprime.go
@@ -3,14 +3,13 @@
  
 package piscine
 
-import "math"
 
 func FindNextPrime(nb int)int{
 	nextprime:=nb-1
 	i:=nb+1
 	for i>nb{
 		nextprime++
-		if premier(nextprime){
+		if IsPrime(nextprime){
 			return nextprime
 		}
 		i++	
@@ -19,15 +18,4 @@ func FindNextPrime(nb int)int{
 }
 
 
-func premier(nb int) bool{
-	decision:=true
-	if nb<=1{
-		return false
-	}
-	for i:=2;i<int(math.Round(math.Sqrt(float64(nb))))+1;i++{
-		if nb%i==0{
-			decision=false
-		}
-	}
-	return decision
-}
+
