3d965dac61c1812b30bf43fda645e47432178cb5
TMendes1 <tomas12001@hotmail.com>
2019-09-12 13:45:35 +0100

    kk2

--- btreetransplant.go
+++ btreetransplant.go
@@ -1,21 +1,19 @@
 package piscine
 
 func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+    a := node
 
-	replace := node
-	
-	if root == nil {
-		return nil
-	}
-
-	if node.Parent == nil {
-		root = rplc
-	} else if node == node.Parent.Left {
-		rplc=replace.Parent.Left 
-	} else {
-		replace.Parent.Right = rplc
-	}
-	node.Parent=replace.Parent 
-
-	return root
+    if node.Parent == nil {
+        root = rplc
+    } else if node == node.Parent.Left {
+        a.Parent.Left = rplc
+    } else {
+        a.Parent.Right = rplc
+    }
+    a.Parent = node.Parent
+    
+    return root
 }
