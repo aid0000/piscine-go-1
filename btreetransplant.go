28e8293bd5012072d3333882809030d5b254ef39
TMendes1 <tomas12001@hotmail.com>
2019-09-12 12:28:26 +0100

    jd

--- btreetransplant.go
+++ btreetransplant.go
@@ -1,6 +1,23 @@
 package piscine
 
 func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	replacement := node
+	if node.Parent == nil {
+		root = rplc
+	} else if node == node.Parent.Left {
+		replacement.Parent.Left = rplc
+	} else {
+		replacement.Parent.Right = rplc
+	}
+	replacement.Parent = node.Parent
+
+	return root
+}
+/*func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
 
 	replace := node
 	
@@ -19,3 +36,4 @@ func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
 
 	return root
 }
+*/
