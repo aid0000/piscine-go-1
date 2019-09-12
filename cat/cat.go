4d0cd1d7135dd5291c3b1a14d8f8824629637cec
TMendes1 <tomas12001@hotmail.com>
2019-09-12 14:01:01 +0100

    cat

--- cat/cat.go
+++ cat/cat.go
@@ -4,17 +4,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	piscine".."
 )
 
 func main() {
-	size := len(os.Args)
-	/*	if size == 1 {
-		for { //could use fmt.scanf but not sure if the student can use it
-			reader := bufio.NewReader(os.Stdin)
-			data, _ := reader.ReadString('\n')
-			fmt.Print(data)
-		}
-	} else if len(os.Args) > 1 {*/
+	size := piscine.Lent3(os.Args)
+	
 	for i := 1; i < size; i++ {
 		data, err := ioutil.ReadFile(os.Args[i])
 		if err != nil {
@@ -25,6 +20,6 @@ func main() {
 		fmt.Println()
 	}
 
-	//}
+	
 
 }
