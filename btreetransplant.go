25871117e7abd01980a3944370010e4d0c7de23e
TMendes1 <tomas12001@hotmail.com>
2019-09-12 12:34:11 +0100

    jd

--- btreetransplant.go
+++ btreetransplant.go
@@ -5,35 +5,15 @@ func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
 		return nil
 	}
 
-	replacement := node
-	if node.Parent == nil {
-		node = rplc
-	} else if node == node.Parent.Left {
-		replacement.Parent.Left = rplc
-	} else {
-		replacement.Parent.Right = rplc
-	}
-	replacement.Parent = node.Parent
-
-	return root
-}
-/*func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
-
-	replace := node
 	
-	if root == nil {
-		return nil
-	}
-
 	if node.Parent == nil {
 		root = rplc
 	} else if node == node.Parent.Left {
-		replace.Parent.Left = rplc
+		node.Parent.Left = rplc
 	} else {
-		replace.Parent.Right = rplc
+		node.Parent.Right = rplc
 	}
-	replace.Parent = node.Parent
+	node.Parent = rplc.Parent
 
 	return root
 }
-*/
