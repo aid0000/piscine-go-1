3cd6460a0e177c210ce707c1103b69de5a9949d5
TMendes1 <tomas12001@hotmail.com>
2019-09-13 15:05:31 +0100

    s

--- comcheck/comchek.go
+++ comcheck/comchek.go
@@ -18,42 +18,10 @@ func main() {
 				if result == s {
 					fmt.Println("Alert!!!")
 					return
-					
+
 				}
 			}
-			
+
 		}
 	}
 }
-/*
-func main() {
-	var arrayRune []rune
-	var result string
-	safeWords := []string{"01", "galaxy", "galaxy 01"}
-
-	for i := 1; i < len(os.Args); i++ {
-		arrayRune = []rune(os.Args[i])
-	}
-
-	for j := 0; j < len(arrayRune); j++ {
-		if arrayRune[j] != ' ' {
-			result += string(arrayRune[j])
-		}
-	}
-
-	for _, s := range safeWords {
-		if result == s {
-			fmt.Println("Alert!!!")
-		}
-	}
-
-
-
-
-	if args[i] == str[k] {
-				fmt.Println("Alert!!!")
-				
-			}else{
-				break
-			}
-}*/
