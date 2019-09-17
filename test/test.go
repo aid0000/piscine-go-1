31066fe78e14e7a349bcbb4c2f82d90ed3f3ca86
TMendes1 <tomas12001@hotmail.com>
2019-09-17 11:03:32 +0100

    jd

--- test/test.go
+++ test/test.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-       "fmt"
-
-       piscine ".."
+	"fmt"
+	piscine ".."
 )
 
-const N = 6
-
 func main() {
-	arr := make([]string, N)
-	arr[0] = "a"
-	arr[2] = "b"
-	arr[4] = "c"
-
-	for _, v := range arr {
-		fmt.Println(v)
-	}
-
-	fmt.Println("Size after compacting:", piscine.Compact(&arr))
-
-	for _, v := range arr {
-		fmt.Println(v)
-	}
+	arr := []int{1, 2, 3, 1, 2, 3, 4}
+	unmatch := piscine.Unmatch(arr)
+	fmt.Println(unmatch)
 }
--- unmach.go
+++ unmach.go
@@ -2,5 +2,20 @@ package piscine
 
 func Unmatch(arr []int) int {
 
-	return 0	
+	for i := 0; i < Lent4(arr); i++ {
+		for j := i + 1; j < Lent4(arr); j++ {
+			if arr[i] == arr[j] {
+				arr[i] = 0
+				arr[j] = 0
+				break
+			}
+		}
+	}
+	for k := 0; k < Lent4(arr); k++ {
+		if arr[k] != 0 {
+
+			return arr[k]
+		}
+	}
+return 0
 }
