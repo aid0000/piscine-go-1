37fc6f9e0177e63d8e1658ca6d7c96042e0886b3
TMendes1 <tomas12001@hotmail.com>
2019-09-17 11:07:32 +0100

    jd

--- unmach.go
+++ unmach.go
@@ -5,14 +5,14 @@ func Unmatch(arr []int) int {
 	for i := 0; i < Lent4(arr); i++ {
 		for j := i + 1; j < Lent4(arr); j++ {
 			if arr[i] == arr[j] {
-				arr[i] = 0
-				arr[j] = 0
+				arr[i] = -1
+				arr[j] = -1
 				break
 			}
 		}
 	}
 	for k := 0; k < Lent4(arr); k++ {
-		if arr[k] != 0 {
+		if arr[k] != -1 {
 
 			return arr[k]
 		}
