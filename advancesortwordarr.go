9fcf378f6f9689af3e9204d11d648bdc0b8da223
TMendes1 <tomas12001@hotmail.com>
2019-09-18 11:18:46 +0100

    advancesortword

--- advancesortwordarr.go
+++ advancesortwordarr.go
@@ -1,15 +1,28 @@
 package piscine
 
+import (
+	//"fmt"
+)
+
+
 func AdvancedSortWordArr(array []string, f func(a, b string) int) {
 	
 	for i:=1; i<Lent3(array) ;i++{
-		if f(array[i], array[i-1]) > 0 {
+		if f(array[i], array[i-1]) > 1 {
 			array[i]=array[i-1]                  //anda uma posicao
 			array[i-1]=array[i]					//recua uma posicao
 			i=0
 		}
-
+		
 	}
+	quickSrot2(array, 0, len(array)-1)
 }
 
-
+/*
+var str string
+for i := 0; i < Lent3(array); i++ {
+	str = str + array[i]
+}
+arr:=array
+quickSrot2(array, 0,f(str, string(len(array)-1)))
+fmt.Println(arr) */
