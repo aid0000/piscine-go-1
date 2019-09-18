df1514cd01e01d61ea546c785aad68ad73ed0660
TMendes1 <tomas12001@hotmail.com>
2019-09-18 10:43:30 +0100

    advancewordsorting

--- advancesortwordarr.go
+++ advancesortwordarr.go
@@ -2,7 +2,7 @@ package piscine
 
 func AdvancedSortWordArr(array []string, f func(a, b string) int) {
 	
-	for i:=0; i<Lent3(array) ;i++{
+	for i:=1; i<Lent3(array) ;i++{
 		if f(array[i], array[i-1]) > 0 {
 			array[i]=array[i-1]                  //anda uma posicao
 			array[i-1]=array[i]					//recua uma posicao
